73b17b1704e49499ea40abd1395067ece66c8415
upendhar Nemmani <upendharnemmani@gmail.com>
2024-09-28 09:55:53 +0530

    V1 - Vercel config deleted

--- src/error.js
+++ src/error.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // redirect to draw.html if screen width is greater than 1024px, otherwise to error.html
   function updateLink() {
     if (window.innerWidth > 1024) {
-      tryNowLink.href = "/draw";
+      tryNowLink.href = "./draw.html";
     } else {
-      tryNowLink.href = "/error";
+      tryNowLink.href = "./error.html";
     }
   }
   updateLink();
